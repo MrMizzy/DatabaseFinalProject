2673b883a99a844a13dcdd11a97c8998cd830908
MrMizzy <99ademidea@gmail.com>
2025-04-09 19:02:09 +0000

    Added Managers table

--- databasefinalproject.sql
+++ databasefinalproject.sql
@@ -1,1 +1,8 @@
-create database Managers
+-- Active: 1744224929379@@127.0.0.1@3306@hostelsystem
+create database HostelSystem;
+
+CREATE Table Managers(
+    `Manager_ID` int NOT NULL PRIMARY KEY,
+    `Name` VARCHAR(255),
+    `PhoneNo.` VARCHAR(13)
+);
