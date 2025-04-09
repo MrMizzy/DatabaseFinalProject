693c090ed5bf457b25e8f30cd01c339ef01959bd
AdebayoGbengaBello <adebayo.bello@ashesi.edu.gh>
2025-04-09 20:17:29 +0000

    Adding Hostel Table databasefinalproject.sql
    I have created the hostel table, with the foreign key manager_ID that references the manager table primary key manager_ID

--- databasefinalproject.sql
+++ databasefinalproject.sql
@@ -6,3 +6,11 @@ CREATE Table Managers(
     `Name` VARCHAR(255),
     `PhoneNo.` VARCHAR(13)
 );
+
+CREATE Table Hostels(
+    Hostel_ID int NOT NULL PRIMARY KEY,
+    Name VARCHAR(255),
+    Location VARCHAR(255),
+    Manager_ID int,
+    FOREIGN KEY(Manager_ID) REFERENCES managers(manager_ID));
+select * from hostels;
